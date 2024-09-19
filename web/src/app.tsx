9d0e7e7e7233b9a80118c53fb9ebfbd80499ddec
Luiz Felipe Santana Dos Santos <fs07santana@gmail.com>
2024-09-18 22:55:00 -0300

    deploy aula 3

--- web/src/app.tsx
+++ web/src/app.tsx
@@ -2,12 +2,48 @@ import { Dialog } from './components/ui/dialog'
 import { CreateGoal } from './components/create-goal'
 import { EmptyGoals } from './components/empty-goals'
 import { Summary } from './components/summary'
+import { useEffect, useState } from 'react'
+import { useQuery } from '@tanstack/react-query'
+import { getSummary } from './http/get-summary'
+
+type SummaryResponse = {
+  completed: number
+  total: number
+  goalsPerDay: Record<
+    string,
+    {
+      id: string
+      title: string
+      completedAt: string
+    }[]
+  >
+}
 
 export function App() {
+  const [summary, setSummary] = useState<SummaryResponse | null>(null)
+
+  // useEffect(() => {
+  //   fetch('http://localhost:3333/summary')
+  //     .then(response => {
+  //       return response.json()
+  //     })
+  //     .then(data => {
+  //       setSummary(data.summary)
+  //     })
+  // }, [])
+
+  const { data } = useQuery<SummaryResponse>({
+    queryKey: ['summary'],
+    queryFn: getSummary,
+    staleTime: 1000 * 60,
+  })
+
   return (
     <Dialog>
+      {data?.total && data.total > 0 ? <Summary /> : <EmptyGoals />}
+
       {/* <EmptyGoals /> */}
-      <Summary />
+      {/* <Summary /> */}
       <CreateGoal />
     </Dialog>
   )
